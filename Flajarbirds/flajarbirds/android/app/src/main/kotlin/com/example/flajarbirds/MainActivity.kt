65afdefb1d5c4c9eaad2247c1fc1e3d189e39dd6
Fjralfath <fjralfath11@gmail.com>
2022-10-04 19:07:02 +0100

    first commit

---
+++ Flajarbirds/flajarbirds/android/app/src/main/kotlin/com/example/flajarbirds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flajarbirds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flajarbirds/flajarbirds/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Flajar());
+}
+
+class Flajar extends StatelessWidget {
+  const Flajar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
